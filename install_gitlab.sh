0ddfec527c01e1164dbbcaf74dace3d0985c8b4f
mudocker <mudocker@163.com>
2018-09-01 17:31:05 +0800

    20180901173105

---
+++ install_gitlab.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+apt-get install git
+adduser git
+mkddir srv
+#https://www.liaoxuefeng.com/wiki/0013739516305929606dd18361248578c67b8067c8c017b000/00137583770360579bc4b458f044ce7afed3df579123eca000
